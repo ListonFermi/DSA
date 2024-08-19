7b7bd6cb8f05963bf93f002d1ff32f4e3bc0bfdf
Liston Fermi <listonfermi@gmail.com>
2024-08-19 12:42:36 +0530

    added bfs, dfs iterative

--- Data Structures/8 Graph/Graph.js
+++ Data Structures/8 Graph/Graph.js
@@ -45,24 +45,43 @@ class Graph {
     return result;
   }
 
-  //BFS
-  breadthFirstSearch(start) {
+  //DFS- Iterative
+  depthFirstSearchIterative(start) {
+    const visited = {};
+    const stack = [start];
+
+    while (stack.length) {
+      const curr = stack.pop();
+
+      if (visited[curr]) continue;
+      console.log(curr);
+
+      for (let neighbor of this.adjacencyList[curr]) {
+        stack.push(neighbor);
+      }
+
+      visited[curr] = true;
+    }
+  }// Time: O(V + E) V- No. of neighbors, E- No. of edges | Space: O(V)
+
+  //BFS- Iterative
+  breadthFirstSearchIterative(start) {
     const visited = {};
     const queue = [start];
 
     while (queue.length) {
       const curr = queue.shift();
-      if (!visited[curr]) {
-        console.log(curr);
 
-        for (let neighbor of this.adjacencyList[curr]) {
-          queue.push(neighbor);
-        }
+      if (visited[curr]) continue;
+      console.log(curr);
 
-        visited[curr] = true;
+      for (let neighbor of this.adjacencyList[curr]) {
+        queue.push(neighbor);
       }
+
+      visited[curr] = true;
     }
-  }
+  }// Time: O(V + E) V- No. of neighbors, E- No. of edges | Space: O(V)
 }
 
 const graph = new Graph();
